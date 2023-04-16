7bd2200c205d8d7cdcbb1242cb1d006f7d3ec039
borisgrd <borisgrd@gmail.com>
2023-04-16 19:58:37 -0400

    log click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,21 @@ import { Fragment } from "react";
 
 function ListGroup() {
   let items = ["NY", "SF", "LA", "WD"];
-  items = [];
+  //items = [];
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(item, index, event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
