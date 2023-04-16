d1f2062f2d27e476a323fc43c7ac08ccfba725aa
borisgrd <borisgrd@gmail.com>
2023-04-16 19:49:45 -0400

    remove ternetary operator

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,13 +4,10 @@ function ListGroup() {
   let items = ["NY", "SF", "LA", "WD"];
   items = [];
 
-  const getMessage = () => {
-    return items.length === 0 ? <p>No items found</p> : null;
-  };
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
