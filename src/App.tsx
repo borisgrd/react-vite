39b4ec07b33f5a4e7413fb1482de264f1de7a10a
borisgrd <borisgrd@gmail.com>
2023-04-22 16:14:45 -0400

    use alert with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 ListGroup;
 
 function App() {
-  let items = ["NY", "SF", "LA", "WD"];
+  // let items = ["NY", "SF", "LA", "WD"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+      <Alert>Hello</Alert>
     </div>
   );
 }
