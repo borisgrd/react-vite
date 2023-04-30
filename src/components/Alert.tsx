473c81e6f06e23ea258d7e8d0d1ee09d1cc160ea
borisgrd <borisgrd@gmail.com>
2023-04-29 20:29:47 -0400

    final step

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,13 +2,20 @@ import React, { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
     <div className="alert alert-primary alert-dismissable">
       {children}
-      <button type="button" class="btn-close" data-bs-dismiss></button>
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
     </div>
   );
 };
